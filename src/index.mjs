0f2220faf60d34bc38e410a92fc29a02152c9148
blwatkins <blwatkins@users.noreply.github.com>
2025-10-30 22:38:22 -0500

    Add initial express app and server

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,7 @@
-console.log('Hello, World!');
+import { app } from './app.mjs';
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`randomness API is listening on port ${PORT}`);
+});
